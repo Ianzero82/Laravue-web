1765f13597b409f67accf36e2dc8e3c589e77cc2
Ian Rosalinda <ian@stlthvape.com>
2023-01-04 17:12:25 -0500

    Code cleanup

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import axios from "axios"
 import './assets/main.css'
 
 axios.defaults.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+axios.defaults.baseURL = "http://127.0.0.1:8002/api/v1/"
+// axios.defaults.baseURL = "http://10.0.0.87:8001/api/v1/"
 
 const app = createApp(App)
 
